339d2bd7564288a562eded8869d460b4685069d1
freedoomiran <alifar3029@gmail.com>
2026-07-25 20:48:54 +0330

    Update server.js

--- backend/models/backend/backend/api/server.js
+++ backend/models/backend/backend/api/server.js
@@ -1,1 +1,6 @@
+app.use(express.json());
+const userRoutes = require("../routes/users");
 
+app.use("/api/users", userRoutes);const userRoutes = require("../routes/users");
+
+app.use("/api/users", userRoutes);
