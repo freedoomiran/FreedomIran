3f815cf6cc225879c0c522db5852e3cb799f7313
freedoomiran <alifar3029@gmail.com>
2026-07-25 20:46:08 +0330

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,1 +1,25 @@
+const users = [];
 
+function createUser(data) {
+    const user = {
+        id: users.length + 1,
+        username: data.username,
+        email: data.email,
+        language: data.language || "fa",
+        location: data.location || null,
+        created_at: new Date()
+    };
+
+    users.push(user);
+
+    return user;
+}
+
+function getUsers() {
+    return users;
+}
+
+module.exports = {
+    createUser,
+    getUsers
+};
