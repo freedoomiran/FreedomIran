545f71c0536e6f238e2be7e4d4185161247d5689
freedoomiran <alifar3029@gmail.com>
2026-07-25 20:46:57 +0330

    Update users.js

--- backend/models/backend/routes/users.js
+++ backend/models/backend/routes/users.js
@@ -1,1 +1,31 @@
+const express = require("express");
+const router = express.Router();
 
+const {
+    createUser,
+    getUsers
+} = require("../models/user");
+
+
+router.post("/", (req, res) => {
+
+    const user = createUser(req.body);
+
+    res.json({
+        message: "User created successfully",
+        user
+    });
+
+});
+
+
+router.get("/", (req, res) => {
+
+    res.json({
+        users: getUsers()
+    });
+
+});
+
+
+module.exports = router;
